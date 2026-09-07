dea9c5a2c2f50d2aad087f5412e70084e96b2115
elcodebase <jehezkiel.jefferson@ui.ac.id>
2026-09-07 22:00:51 +0700

    Fix error karena conflict branch

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -161,7 +161,4 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 WHITENOISE_USE_FINDERS = True
-<<<<<<< HEAD
 
-=======
->>>>>>> 9f98c0de9a76dc1aa7d5f027c0d6f3d78eca2e60
